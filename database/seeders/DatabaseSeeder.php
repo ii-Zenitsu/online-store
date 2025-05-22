85bcff3119fa4279a520ca61ff065dbf2a5d462c
samira <khindoufsamira707@gmail.com>
2025-05-22 16:52:29 +0100

    tache G

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@mail.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'is_super_admin' => true,
+        ]);
     }
 }
